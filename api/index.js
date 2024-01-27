403021bd7fe4c5026c4f3884ce22c47321c4d6c3
Ceasar2001 <ecleoceasar1@gmail.com>
2024-01-27 16:10:42 +0800

    database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-const app = express();
+dotenv.config();
 
-app.listen(3000, () => {
-    console.log('server running on port 3000');
-})
+mongoose.connect(process.env.MONGO)
+.then(() => {
+    console.log('mongodb is connected');
+}).catch(err => {
+    console.log(err);
+});
+
+const app = express(); 
